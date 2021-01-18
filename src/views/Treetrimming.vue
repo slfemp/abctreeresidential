cf1d31c631b90acbaf8a087777f3041db5b14133
mICHAEL mOORE <mike@slfemp.com>
2021-01-18 09:49:00 -0600

    update2

--- src/views/Treetrimming.vue
+++ src/views/Treetrimming.vue
@@ -24,21 +24,49 @@
         <div class="row">
 
         <div class="col-5">
-          <h4>Common Reasons for Tree Removal</h4>
+          <h4>Tree Trimming Services</h4>
              <ul class="list-style-4">
-                      <li>Dead Trees</li>
+                      <li>Pruning</li>
+                       <li>Topping</li>
+                        <li>Crown Thinning</li>
               </ul>
          </div>
 
          <div class="col-7">
-           <h3>Dead Trees</h3>
-           <p>Tree trimming preserves the shape of your trees,
-             opens the crown for air and sunlight circulation,
-             eliminates problem sources, and helps keep your trees
-             beautiful and healthy. Our bucket trucks have a 7</p>
+           <h3>Pruning</h3>
+           <p>Tree topping is a practice used with some trees to
+             help to promote proper plant growth, safety and to
+             maintain their beauty. Old dead branches can be a
+             hazard to your home, or even injure a passerby.
+             Don’t let your trees overgrow powerlines, lights,
+             paths, or any other object in your landscape. By
+             taking off the top of the tree, we can reduce the
+             amount of shade the tree gives, providing you opportunities
+             you don’t have in the shade of a large tree. Plants underneath
+             your tree will begin to thrive in sunlight, lawns will flourish again.</p>
 
-            <h3>Remove Unwanted Branches</h3>
-            <p></p>
+            <h3>Topping</h3>
+            <p>Tree topping is a practice used with some trees to help
+              to promote proper plant growth, safety and to maintain
+              their beauty. Old dead branches can be a hazard to
+              your home, or even injure a passerby. Don’t let your
+              trees overgrow powerlines, lights, paths, or any other
+              object in your landscape. By taking off the top of the
+              tree, we can reduce the amount of shade the tree gives,
+              providing you opportunities you don’t have in the shade of a
+              large tree. Plants underneath your tree will begin to
+              thrive in sunlight, lawns will flourish again.</p>
+
+               <h3>Topping</h3>
+               <p>If you have large trees on your property,
+                 you may need to begin thinning the crown or
+                 canopy of the tree year after year.
+                 When performed properly, thinning your
+                 tree will improve its health, its beauty,
+                 and it can provide you with a myriad of other
+                 benefits. We have the proper experience, tools,
+                 and knowledge to ensure that your tree thrives
+                 for years to come.</p>
 
           </div>
          </div>
