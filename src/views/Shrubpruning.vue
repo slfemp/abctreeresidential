a4fc7d6e8edd6a685b8f21c06d084837295cb857
mICHAEL mOORE <mike@slfemp.com>
2021-01-18 09:59:21 -0600

    update4

--- src/views/Shrubpruning.vue
+++ src/views/Shrubpruning.vue
@@ -26,19 +26,31 @@
         <div class="col-5">
           <h4>Common Reasons for Shrub Pruning</h4>
              <ul class="list-style-4">
-                      <li>Dead Trees</li>
+                      <li>Beautity your property</li>
+                      <li>Healthier Plants</li>
+                      <li>Fuller Shrubs</li>
+                      <li>Removed Diseases Parts of The Shrud</li>
               </ul>
          </div>
 
          <div class="col-7">
-           <h3>Dead Trees</h3>
-           <p>Tree trimming preserves the shape of your trees,
-             opens the crown for air and sunlight circulation,
-             eliminates problem sources, and helps keep your trees
-             beautiful and healthy. Our bucket trucks have a 7</p>
-
-            <h3>Remove Unwanted Branches</h3>
-            <p></p>
+           <h3>Shrub Pruning promotes tree health.</h3>
+           <p>Pruning is a horticultural and silvicultural practice
+             involving the selective removal of certain parts of a plant,
+             such as branches, buds, or roots. The practice entails
+             targeted removal of diseased, damaged, dead, non-productive,
+             structurally unsound, or otherwise unwanted tissue from crop
+             and landscape plants. In general, the smaller the branch that is cut,
+             the easier it is for a woody plant to compartmentalize the wound
+             and thus limit the potential for pathogen intrusion
+             and decay. It is therefore preferable to make any
+             necessary formative structural pruning cuts to young plants,
+             rather than removing large, poorly placed branches from mature plants.
+           </p>
+             <p>
+              Ensure the growth and long-term health of your
+              shrubs with ABC Professional Tree Services.
+            </p>
 
           </div>
          </div>
